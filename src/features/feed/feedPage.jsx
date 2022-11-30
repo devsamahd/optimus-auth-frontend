16cb3c99f440a5663321dd8c7e96e573d5c4d595
devsamahd <devsamahd@gmail.com>
2022-11-30 22:20:55 +0100

    added dashboard placeholder

--- src/features/feed/feedPage.jsx
+++ src/features/feed/feedPage.jsx
@@ -1,6 +1,5 @@
 import {useSelector} from "react-redux"
 import {selectCurrentUser } from "../auth/authSlice"
-import {Link} from "react-router-dom"
 
 const Feed = () => {
     const user = useSelector(selectCurrentUser)
@@ -8,7 +7,17 @@ const Feed = () => {
     <div>
       <div className="row">
         <div className="col-md-1"></div>
-        <div className="col-md-10 box"></div>
+        <div className="col-md-10 box">
+          <div className="row">
+
+            <div className="col-md-6">
+              <h1>WaWu! you made it.</h1>
+              <p>This page will be designed later thank you</p>
+            </div>
+            <div className="col-md-6"></div>
+
+          </div>
+        </div>
         <div className="col-md-1"></div>
       </div>
     </div>
